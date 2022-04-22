8a5c4cf89448f21d55d5658af94a101c4fe30733
Vladyas <yaskevich@mail.ru>
2022-04-23 00:23:46 +0300

    correct driver_test.py was added from newer branch

--- driver_test.py
+++ driver_test.py
@@ -9,9 +9,14 @@ if __name__ == '__main__':
     sys.argv.append('-e')
     sys.argv.append(f_in)
     main()
+    print(f'Encoding time:{time.time_ns() - time_start} ns')
+    time_start = time.time_ns()
     sys.argv[1] = '-d'
     sys.argv[2] = f_encoded
     main()
+    print(f'Decoding time:{time.time_ns() - time_start} ns')
+    time_start = time.time_ns()
+
     with open(f_in, 'r', encoding='utf-8') as f1:
         with open(f_decoded, 'r', encoding='utf-8') as f2:
             src = f1.read()
@@ -20,4 +25,4 @@ if __name__ == '__main__':
                 print('ERROR: the source and decoded result are not identical!')
             else:
                 print('OKAY!')
-    print(f'Execution time:{time.time_ns() - time_start} ms')
+    print(f'File comparing time:{time.time_ns() - time_start} ns')
