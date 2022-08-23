a3157643db76858350afa8ab4f2389d6143a25bf
Vladyas <yaskevich@mail.ru>
2022-08-23 17:55:29 +0300

    parametrize test_unit.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest, sys
 from src import driver
 from src import huffman_codec
@@ -10,14 +12,32 @@ PARAM_DECODE = 'd'
 books = [('War_and_Peace.txt', 'War_and_Peace_e.txt', 'War_and_Peace_e_d.txt'),
          ('chinese.txt', 'chinese_e.txt', 'chinese_e_d.txt')]
 
+# unnit_test  fixtures params
+# codding to test is got from https://www.csfieldguide.org.nz/en/interactives/huffman-tree/
+inputs_encodes_freqs=[
+    ('aaaaaaaaaaeeeeeeeeeeeeeeeiiiiiiiiiiiiooouuuussssssssssssst',
+     {'i': '00', 's': '01', 'e': '10', 'u': '1100', 't': '11010', 'o': '11011','a': '111'},
+     {'a': 10, 'e': 15, 'i': 12, 'o': 3, 'u': 4, 's': 13, 't': 1}),
+    ('Привет кодировщик Хаффмана!!',
+     {'и':'001', 'м':'0000', 'н':'0001', 'а':'010', 'р':'0110','в':'0111', ' ':'1000', 'к':'1001', 'о':'1010',
+      'ф':'1011', '!':'1100', 'П':'11010', 'е':'11011', 'т':'11100', 'д':'11101', 'щ':'11110','Х':'11111' },
+     {'и':3, 'м': 1, 'н':1, 'а':3, 'р':2, 'в': 2, ' ':2, 'к':2, 'о':2,
+      'ф':2, '!': 2, 'П': 1, 'е': 1, 'т': 1, 'д': 1, 'щ': 1, 'Х': 1}
+     )
+]
 
+ids = []
+@pytest.fixture(params=inputs_encodes_freqs)
+def get_encode_freqs_dicts(request):
+    TMP_FILE  = 'tmp.txt'
+    with open(TMP_FILE, 'w') as f:
+        f.write(request.param[0])
 
-def _prepare_huffman_algorithm():
     # https: // www.csfieldguide.org.nz / en / interactives / huffman - tree /
     hc = HuffmanCodec()
     hc.ha = HuffmanAlgorithm()
     f_input_lenght = 0
-    with open('1.txt', 'r', encoding='utf-8') as f_input:
+    with open(TMP_FILE, 'r', encoding='utf-8') as f_input:
         while True:
             buff = f_input.read(hc.buf_size)
             f_input_lenght += len(buff)
@@ -27,12 +47,10 @@ def _prepare_huffman_algorithm():
                 break
 
     hc.ha.prepare_encoding_alg()
-    return hc.ha.encode, hc.ha.freq
+    yield {'test_encode':hc.ha.encode, 'test_freq':hc.ha.freq,
+           'correct_encode':request.param[1], 'correct_freq':request.param[2]}
 
-
-@pytest.fixture()
-def get_encode_freqs_dicts():
-    yield _prepare_huffman_algorithm()
+    os.remove(TMP_FILE)
 
 
 @pytest.fixture(params=books)
